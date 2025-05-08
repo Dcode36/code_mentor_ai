ead0bb4a488a4d53c9a40417a630dce20ef55d9e
Tejas Patil <tejaspatil@Tejass-Mac.local>
2025-05-09 04:23:47 +0530

    final commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import App from './App';
 import './index.css';
 import { ClerkProvider } from '@clerk/clerk-react'
 
-<<<<<<< HEAD
-=======
-
->>>>>>> tejas
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -16,11 +12,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   
   <React.StrictMode>
-<<<<<<< HEAD
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-=======
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
->>>>>>> tejas
       <App />
     </ClerkProvider>
   </React.StrictMode>
